d6045815dbb21f7fb1a8e04d97f8ac7827347ef6
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-04-15 02:38:26 -0300

    [Parte 3.4] Aplicando el sistema de templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,6 +22,7 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
+#error 404
 #def detail(request, question_id):
 #    try:
 #        question = Question.objects.get(pk=question_id)
