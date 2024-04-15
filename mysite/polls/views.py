bb734c7c45c861fbb7213c29b753fd97a31e7588
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-04-15 02:30:57 -0300

    [Parte 3.3] Introducción al error 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
@@ -20,3 +21,15 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, "polls/detail.html", {"question": question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
